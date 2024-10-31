cf5f6678c495cdf03d8ab4bfb6ae02fdf59fbedd
Tyler Stewart <stewartt5@leidos.com>
2024-10-31 14:59:50 -0400

    Implemented exit strategy for user, removed half of the code (redundant); handled most of it using Move/Copy variables

--- MassiveExtensionFolderCopy.ps1
+++ MassiveExtensionFolderCopy.ps1
@@ -10,122 +10,74 @@ function Optimize-Path {
 }
 
 # Use optimize function to normalize folder paths
-$CopyCutChoice = Read-Host "Enter cut to move all found files from origin to destination. Enter or anything else to continue copying."
-$OriginalFolder = Optimize-Path (Read-Host -Prompt "Enter the folder path to copy/move from (ex: C:\Test)")
-$DestinationFolder = Optimize-Path (Read-Host -Prompt "Enter the destination folder path to copy/move files to (ex: C:\Destination)")
-$FileType = Read-Host -Prompt "Please provide the file type you want to copy/move from the original folder. If it's a screenshot (*.png), type 'docx'. If a screen recording/downloaded video, enter mp4. Go ahead"
+$CopyCutChoice = Read-Host "Enter cut to move files from origin to destination. E to exit (following prompts), or any key to copy."
+    $MovingOrCopying = if ($CopyCutChoice.Trim().ToLower() -eq "cut") { "Moving" } else { "Copying" }
+    $MoveOrCopy = if ($MovingOrCopying -eq "Moving") { "move" } else { "copy"}
+    $MoveOrCopyCommand = if ($MovingOrCopying.Trim().ToLower() -eq "moving") { "Move-Item" } else { "Copy-Item" }
+$OriginalFolder = Optimize-Path (Read-Host -Prompt "Enter the folder path to $MoveOrCopy from (ex: C:\Test)")
+$DestinationFolder = Optimize-Path (Read-Host -Prompt "Enter the destination folder path to $MoveOrCopy files to (ex: C:\Destination)")
+$FileType = Read-Host -Prompt "Please provide the file type you want to $MoveOrCopy from the original folder. If it's a screenshot (*.png), type 'docx'. If a screen recording/downloaded video, enter mp4. Go ahead"
 
-# ISSUE-INCOMPLETE -> DO LATER      # $CopyCutChoice = Read-Host -Prompt "Enter Cut to cut files, or enter/any other key to continue. Type e to exit."
 # save all errors for end using string array (if length less than 0, print no errors found)
 #add subdirectory support for copying subdirectories with certain file types (for onedrive)
 
-if($CopyCutChoice.ToLower() -eq "cut")
+# Exits if user had selected
+if($CopyCutChoice.ToLower().Trim() -ne "e") 
 {
-    # Check if paths exist
-    if ((Test-Path -LiteralPath $OriginalFolder) -and (Test-Path -LiteralPath $DestinationFolder)) {
-        # Get all files of the specified type from the original folder
-        $allFiles = Get-ChildItem -LiteralPath $OriginalFolder -Filter "*.$FileType" -File -Recurse
-        $movedAllFilesSuccessfully = $true # Tracks if all files were moved successfully
+# Check if bothpaths exist
+if ((Test-Path -LiteralPath $OriginalFolder) -and (Test-Path -LiteralPath $DestinationFolder)) {
+    # Get all files of the specified type from the original folder
+    $allFiles = Get-ChildItem -LiteralPath $OriginalFolder -Filter "*.$FileType" -File -Recurse
+    $transitionedAllFilesSuccessfully = $true # Tracks if all files were transitioned successfully
 
-        if ($allFiles.Count -gt 0) {
-            # Move each file directly to the destination folder
-            for($i = 0; $i -lt $allFiles.Count; $i++)
-            {
-                # Declares current file (not using for each loop in order to write progress)
-                $file = $allFiles[$i]
+    if ($allFiles.Count -gt 0) {
+        # Move or copy each file directly to the destination folder
+        for($i = 0; $i -lt $allFiles.Count; $i++)
+        {
+            # Current file being processed (not using for each loop in order to write progress)
+            $file = $allFiles[$i]
 
-                # Define the destination path as the destination folder + file name only (no subdirectories)
-                $destinationPath = Join-Path -Path $DestinationFolder -ChildPath $file.Name
+            # Define the destination path as the destination folder + file name only (no subdirectories)
+            $destinationPath = Join-Path -Path $DestinationFolder -ChildPath $file.Name
 
-                # Tries to move/copy items over
-                try {
-                    Move-Item -LiteralPath $file.FullName -Destination $destinationPath -Force
-                    # Write-Host "Moved: $($file.Name) to $destinationPath" -ForegroundColor Green
-                }
-                catch {
-                    Write-Host "Failed to move: $($file.Name) to $destinationPath" -ForegroundColor Red
-                    Write-Host "Error message: $_" -ForegroundColor Red
-                    $movedAllFilesSuccessfully = $false
-                }
-
-                # Calculate+round percentage complete and shows progress bar
-                $percentComplete = [math]::Round(($i + 1) / $allFiles.Count * 100)
-                Write-Progress -Activity "Moving Files" -Status "$percentComplete% Complete" -PercentComplete $percentComplete
+            # Tries to move/copy items over
+            try {
+                & $MoveOrCopyCommand -LiteralPath $file.FullName -Destination $destinationPath -Force
+                # Write-Host "$MovingOrCopying: $($file.Name) to $destinationPath" -ForegroundColor Green
+            }
+            catch {
+                Write-Host "Failed to $MoveOrCopy $($file.Name) to $destinationPath" -ForegroundColor Red
+                Write-Host "Error message: $_" -ForegroundColor Red
+                $transitionedAllFilesSuccessfully = $false
             }
 
-            # Checks if all files were successfully moved
-            if ($movedAllFilesSuccessfully -eq $true) {
-                Write-Host "Successfully moved $($allFiles.Count) '*.$FileType' files to '$DestinationFolder'."
-            }
-            else {
-                Write-Host "Some files failed to move. Please try again. Total amount of $FileType files handled: $($allFiles.Count)"
-            }
+            # Calculate+round percentage complete and shows progress bar
+            $percentComplete = [math]::Round(($i + 1) / $allFiles.Count * 100)
+            Write-Progress -Activity "$MovingOrCopying Files" -Status "$($file.Name)); $percentComplete% Complete" -PercentComplete $percentComplete
+
+        }
+
+        # Checks if all files were successfully moved/copied
+        if ($transitionedAllFilesSuccessfully) {
+            Write-Host "$MovingOrCopying $($allFiles.Count) of '*.$FileType' files to '$DestinationFolder' was a success."
         }
         else {
-            Write-Host "No files of type '*.$FileType' were found in '$OriginalFolder'."
+            Write-Host "Some files have failed to $MoveOrCopy. Total amount of $FileType files $MovingOrCopying`: $($allFiles.Count)"
         }
     }
     else {
-        if (-not (Test-Path -LiteralPath $OriginalFolder)) {
-            throw "The path '$OriginalFolder' does NOT exist. Please try again."
-        }
-        if (-not (Test-Path -LiteralPath $DestinationFolder)) {
-            throw "The path '$DestinationFolder' does NOT exist. Please try again."
-        }
+        Write-Host "No files of type '*.$FileType' were found in '$OriginalFolder'."
     }
 }
-# Copies files instead of moving
 else {
-    # Check if paths exist
-    if ((Test-Path -LiteralPath $OriginalFolder) -and (Test-Path -LiteralPath $DestinationFolder)) {
-        # Get all files of the specified type from the original folder
-        $allFiles = Get-ChildItem -LiteralPath $OriginalFolder -Filter "*.$FileType" -File -Recurse
-        $copiedAllFilesSuccessfully = $true # Tracks if all files were copied successfully
-
-        if ($allFiles.Count -gt 0) {
-            # For each file, copy directly to the destination folder
-            for($i = 0; $i -lt $allFiles.Count; $i++)
-            {
-                # Declares current file (not using for each loop in order to write progress)
-                $file = $allFiles[$i]
-
-                # Define the destination path as the destination folder + file name only (no subdirectories)
-                $destinationPath = Join-Path -Path $DestinationFolder -ChildPath $file.Name
-
-                # Tries to copy items over
-                try {
-                    Copy-Item -LiteralPath $file.FullName -Destination $destinationPath -Force
-                    # Write-Host "Copied: $($file.Name) to $destinationPath" -ForegroundColor Green
-                }
-                catch {
-                    Write-Host "Failed to copy: $($file.Name) to $destinationPath" -ForegroundColor Red
-                    Write-Host "Error message: $_" -ForegroundColor Red
-                    $copiedAllFilesSuccessfully = $false
-                }
-
-                # Calculate percentage complete and shows progress bar
-                $percentComplete = [math]::Round(($i + 1) / $allFiles.Count * 100)
-                Write-Progress -Activity "Copying Files" -Status "$percentComplete% Complete" -PercentComplete $percentComplete
-            }
-
-            # Checks if all files were successfully copied
-            if ($copiedAllFilesSuccessfully -eq $true) {
-                Write-Host "Successfully copied $($allFiles.Count) '*.$FileType' files to '$DestinationFolder'."
-            }
-            else {
-                Write-Host "Some files failed to copy. Please try again. Total amount of $FileType files handled: $($allFiles.Count)"
-            }
-        }
-        else {
-            Write-Host "No files of type '*.$FileType' were found in '$OriginalFolder'."
-        }
+    if (-not (Test-Path -LiteralPath $OriginalFolder)) {
+        throw "The path '$OriginalFolder' does NOT exist. Please try again."
     }
-    else {
-        if (-not (Test-Path -LiteralPath $OriginalFolder)) {
-            throw "The path '$OriginalFolder' does NOT exist. Please try again."
-        }
-        if (-not (Test-Path -LiteralPath $DestinationFolder)) {
-            throw "The path '$DestinationFolder' does NOT exist. Please try again."
-        }
+    if (-not (Test-Path -LiteralPath $DestinationFolder)) {
+        throw "The path '$DestinationFolder' does NOT exist. Please try again."
     }
+}
+}
+else {
+    Write-Host "Quitting..."
 }
